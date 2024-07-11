a2f301008b64e2ee5fce9837eab45ee60c43d8d6
SunshineDN <sunshinedn2003@gmail.com>
2024-07-11 10:45:18 -0300

    scroll bug solved

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,34 @@ import Feedback from './components/Feedback/Feedback';
 import Partners from './components/Partners/Partners';
 import Clients from './components/Clients/Clients';
 import Footer from './components/Footer/Footer';
+import { useEffect } from 'react';
 
 const Container = styled.div`
   width: 100%;
 `;
 
+
 function App() {
+  useEffect(() => {
+    // Adiciona um ouvinte de evento para o carregamento completo da página
+    const handleLoad = () => {
+      const hash = window.location.hash;
+      if (hash) {
+        const section = document.querySelector(hash);
+        if (section) {
+          section.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    };
+
+    window.addEventListener('load', handleLoad);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
+  }, []);
+
   return (
     <Container>
       <Header />
